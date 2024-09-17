a520ddf359057747a85373d002718c2c80a93b67
lanceberge <bergeron.lance6@gmail.com>
2024-09-16 20:56:06 -0400

    cleanup to work better with pandoc

--- populate_s3.sh
+++ populate_s3.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-## TODO make the script runnable in any dir
-
 AWS_S3_BUCKET="s3://braindump-bucket"
 
 git ls-files | while read -r file; do
 
-        if [[ "$file" == *.org ]]; then
-                HTML_FILE=${file%.org}.html
-                pandoc "$file" -o "$HTML_FILE"
-                file="$HTML_FILE"
-        fi
+    if [[ "$file" == *.org ]]; then
+        HTML_FILE=${file%.org}.html
+        pandoc "$file" -o "$HTML_FILE"
+        file="$HTML_FILE"
+    fi
 
-        aws s3 cp "$file" "$AWS_S3_BUCKET/$file"
+    aws s3 cp "$file" "$AWS_S3_BUCKET/$file"
 done
 
 find "." -type f -name "*.html" -print0 | xargs -0 rm
